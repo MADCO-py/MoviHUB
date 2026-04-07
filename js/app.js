68266f6d4703235af43f210463b36c1595eb1346
MADCO-py <1782madco@gmail.com>
2026-04-07 09:48:10 -0600

    creacion tipo redit 1

---
+++ js/app.js
@@ -0,0 +1,84 @@
+async function init(){
+
+allPosts = await getPosts();
+render();
+
+}
+
+function render(){
+
+const start = (currentPage-1)*postsPerPage;
+const end = start + postsPerPage;
+
+const pagePosts = allPosts.slice(start,end);
+
+renderPosts(pagePosts);
+renderPagination(allPosts.length);
+
+}
+
+function goPage(p){
+currentPage = p;
+render();
+}
+
+document.getElementById("createBtn").onclick = async ()=>{
+
+const title = document.getElementById("titleInput").value;
+const body = document.getElementById("bodyInput").value;
+
+if(title.length < 3 || body.length < 5){
+alert("Texto muy corto");
+return;
+}
+
+await createPost(title,body);
+
+alert("Post creado");
+
+};
+
+function deletePost(id){
+
+allPosts = allPosts.filter(p=>p.id !== id);
+render();
+
+}
+
+function editPost(id){
+
+const newTitle = prompt("Nuevo título");
+
+allPosts = allPosts.map(p=> p.id===id ? {...p,title:newTitle}:p);
+
+render();
+
+}
+
+function likePost(id){
+alert("Like ❤️");
+}
+
+function viewDetail(id){
+
+const post = allPosts.find(p=>p.id===id);
+
+document.getElementById("posts").innerHTML = `
+<h2>${post.title}</h2>
+<p>${post.body}</p>
+<button onclick="render()">Volver</button>
+`;
+
+}
+
+document.getElementById("searchInput").addEventListener("input",(e)=>{
+
+const text = e.target.value.toLowerCase();
+
+const filtered = allPosts.filter(p=> p.title.toLowerCase().includes(text));
+
+renderPosts(filtered);
+
+});
+
+init();
